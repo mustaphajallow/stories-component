6e3666acf98cc96600354bd2d29f266927bba9bd
mustaphajallow <mustaphajallow369@gmail.com>
2022-02-10 18:21:26 +0000

    stories component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+  
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+        <div className="stories-component">
+          <img src="https://images.unsplash.com/photo-1433086966358-54859d0ed716?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bmF0dXJlJTIwc2VhJTIwNTAwcHglMjBieSUyMDUwMHB4fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60" alt="" className="slider" />
+          <img src="https://images.unsplash.com/photo-1509316975850-ff9c5deb0cd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bmF0dXJlJTIwc2VhJTIwNTAwcHglMjBieSUyMDUwMHB4fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60" alt="" className="slider" />
+          <img src="https://images.unsplash.com/photo-1446071103084-c257b5f70672?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fG5hdHVyZSUyMHNlYSUyMDUwMHB4JTIwYnklMjA1MDBweHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60" alt="" className="slider" />
+          <img src="https://images.unsplash.com/photo-1503435980610-a51f3ddfee50?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fG5hdHVyZSUyMHNlYSUyMDUwMHB4JTIwYnklMjA1MDBweHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60" alt="" className="slider" />
+          <img src="https://images.unsplash.com/photo-1513836279014-a89f7a76ae86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bmF0dXJlJTIwc2VhJTIwNTAwcHglMjBieSUyMDUwMHB4fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60" alt="" className="slider" />
+          <img src="https://images.unsplash.com/photo-1482938289607-e9573fc25ebb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG5hdHVyZSUyMHNlYSUyMDUwMHB4JTIwYnklMjA1MDBweHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60" alt="" className="slider" />
+          <img src="https://images.unsplash.com/photo-1446071103084-c257b5f70672?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fG5hdHVyZSUyMHNlYSUyMDUwMHB4JTIwYnklMjA1MDBweHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60" alt="" className="slider" />
+        </div>
     </div>
   );
 }
---
+++ src/stories.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Stories () {
+  return (
+    <div className="App">
+       hello st
+    </div>
+  );
+}
+
+export default Stories ;
